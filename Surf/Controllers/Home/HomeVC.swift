608644123632d644fc0cba928bcd75d2943d7bd7
liming8964 <liming8964@hotmail.com>
2017-12-04 22:23:25 +0800

    Videos page

--- Surf/Controllers/Home/HomeVC.swift
+++ Surf/Controllers/Home/HomeVC.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class HomeVC: UIViewController {
-
+class HomeVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.collectionView.contentInset = UIEdgeInsetsMake(-20, 0, 0, 0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,4 +35,39 @@ class HomeVC: UIViewController {
     }
     */
 
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        var cellId = ""
+        
+        switch indexPath.row {
+        case 0:
+            cellId = "eventCell"
+            break
+        case 1:
+            cellId = "channelCell"
+            break
+        case 2:
+            cellId = "storyCell"
+            break
+        case 3:
+            cellId = "galleryCell"
+            break
+        default:
+            break
+        }
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize.init(width: self.collectionView.bounds.size.width, height: self.collectionView.bounds.size.height)
+    }
+    
+    
 }
--- Surf/Controllers/Videos/VideosVC.swift
+++ Surf/Controllers/Videos/VideosVC.swift
@@ -7,13 +7,27 @@
 //
 
 import UIKit
+import XLPagerTabStrip
 
-class VideosVC: UIViewController {
-
+class VideosVC: ButtonBarPagerTabStripViewController {
+    
+    var isReload = false
+    
     override func viewDidLoad() {
+        
+        self.settings.style.buttonBarTop = 40
+        self.settings.style.selectedBarHeight = 3
+        self.settings.style.selectedBarBackgroundColor = UIColor.white
+        self.settings.style.buttonBarBackgroundColor = UIColor.clear
+        self.settings.style.buttonBarItemBackgroundColor = UIColor.clear
+        self.settings.style.buttonBarMinimumInteritemSpacing = 0
+        self.settings.style.buttonBarItemTitleColor = UIColor.white
+        self.settings.style.buttonBarItemFont = UIFont.init(name: "Brandon Grotesque", size: 8)!
+        
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        self.buttonBarView.backgroundColor = UIColor.clear
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,5 +45,39 @@ class VideosVC: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // MARK: - PagerTabStripDataSource
+    
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+        let child_0 = self.storyboard?.instantiateViewController(withIdentifier: "FeaturedBarVC")
+        let child_1 = self.storyboard?.instantiateViewController(withIdentifier: "LiveBarVC")
+        let child_2 = self.storyboard?.instantiateViewController(withIdentifier: "ReleaseBarVC")
+        
+        guard isReload else {
+            return [child_0!, child_1!, child_2!]
+        }
+        
+        var childViewControllers = [child_0, child_1, child_2]
+        
+        for index in childViewControllers.indices {
+            let nElements = childViewControllers.count - index
+            let n = (Int(arc4random()) % nElements) + index
+            if n != index {
+                childViewControllers.swapAt(index, n)
+            }
+        }
+        let nItems = 1 + (arc4random() % 8)
+        return Array(childViewControllers.prefix(Int(nItems))) as! [UIViewController]
+    }
+    
+    override func reloadPagerTabStripView() {
+        isReload = true
+        if arc4random() % 2 == 0 {
+            pagerBehaviour = .progressive(skipIntermediateViewControllers: arc4random() % 2 == 0, elasticIndicatorLimit: arc4random() % 2 == 0 )
+        } else {
+            pagerBehaviour = .common(skipIntermediateViewControllers: arc4random() % 2 == 0)
+        }
+        super.reloadPagerTabStripView()
+    }
 
 }
